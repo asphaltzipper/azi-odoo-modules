fb36323f3ab89541581494aeebff85983544061e
scosist <scosist@asphaltzipper.com>
2016-03-03 00:24:06 -0700

    [FIX] mrp: tot_procs and procurements check
    fix tot_procs list to include outbound procurements
    exclude procurements check, appears to have been dropped from
    _procure_orderpoint_confirm in 9.0
    clean up comments

--- mrp_procurement_only/mrp_procurement_only.py
+++ mrp_procurement_only/mrp_procurement_only.py
@@ -13,11 +13,13 @@ _logger = logging.getLogger(__name__)
 class stock_warehouse_orderpoint(models.Model):
     _inherit = 'stock.warehouse.orderpoint'
 
+    # override stock/stock:subtract_procurements_from_orderpoints
     def subtract_procurements_from_orderpoints(self, cr, uid, orderpoint_ids, context=None):
         '''This function returns quantity of product that needs to be deducted from the orderpoint computed quantity because there's already a procurement created with aim to fulfill it.
         We are also considering procurement-only demand for the product and subtracting it from the returned quantity. This method could return a negative quantity, assuming that there is more demand than supply.
         '''
 
+        # only consider procurements within current plan step (context['to_date'])
         cr.execute("""select op.id, p.id, p.product_uom, p.product_qty, pt.uom_id, sm.product_qty, p.origin from procurement_order as p left join stock_move as sm ON sm.procurement_id = p.id,
                                     stock_warehouse_orderpoint op, product_product pp, product_template pt
                                 WHERE p.orderpoint_id = op.id AND p.state not in ('done', 'cancel') AND (sm.state IS NULL OR sm.state not in ('draft'))
@@ -26,15 +28,7 @@ class stock_warehouse_orderpoint(models.Model):
                                 AND p.date_planned <= %s
                                 ORDER BY op.id, p.id
                    """, (tuple(orderpoint_ids), context.get('to_date', datetime.max),))
-                   #""", (tuple(orderpoint_ids), context.get('to_date', '9999-01-01'),))
-                   #""", (tuple(orderpoint_ids), datetime.now()))
-                   #""", (tuple(orderpoint_ids), context.get('to_date', datetime.now())))
         results = cr.fetchall()
-        #if 2 in orderpoint_ids and context.get('to_date', '9999-01-01')>'2016-03-17':
-        #    cr.execute("select * from procurement_order")
-        #    test = cr.fetchall()
-        #    import pdb
-        #    pdb.set_trace()
         current_proc = False
         current_op = False
         uom_obj = self.pool.get("product.uom")
@@ -50,6 +44,7 @@ class stock_warehouse_orderpoint(models.Model):
             proc = move_result[1]
             if proc != current_proc:
                 if 'OUT/' in move_result[6]:
+                    # subtract outbound procurements for production
                     op_qty -= uom_obj._compute_qty(cr, uid, move_result[2], move_result[3], move_result[4], round=False)
                 else:
                     op_qty += uom_obj._compute_qty(cr, uid, move_result[2], move_result[3], move_result[4], round=False)
@@ -59,36 +54,6 @@ class stock_warehouse_orderpoint(models.Model):
         if current_op:
             res[current_op] = op_qty
         return res
-
-                                #-- AND p.date_planned <= %s
-                                #-- AND p.origin NOT LIKE '%OUT/%'
-                   #""", (tuple(orderpoint_ids), datetime.now()))
-                   #""", (tuple(orderpoint_ids), context.get('to_date', datetime.now())))
-    # stock/stock:subtract_procurements_from_orderpoints
-#    def subtract_procurements_from_orderpoints(self, cr, uid, orderpoint_ids, context=None):
-#        res = super(stock_warehouse_orderpoint, self).subtract_procurements_from_orderpoints(cr, uid, orderpoint_ids, context=context)
-#        uom_obj = self.pool.get('product.uom')
-#        op_obj = self.pool.get('stock.warehouse.orderpoint')
-#        proc_obj = self.pool.get('procurement.order')
-#        dom_planned = context.get('to_date', False) and [('date_planned', '<=', context.get('to_date'))] or []
-##        res_planned = {}
-#        res_planned = res
-#        # only consider procurements within current plan step
-##        for op in res:
-##            dom = dom_planned + [('product_id', '=', op_obj.browse(cr, uid, op).product_id.id)]
-##            proc_ids = proc_obj.search(cr, uid, dom)
-##            for proc in proc_obj.browse(cr, uid, proc_ids, context=context):
-##                res_planned[op] = res[op]
-#        # subtract outbound procurements for production
-#        dom = dom_planned + [('product_id', '=', op_obj.browse(cr, uid, orderpoint_ids).product_id.id), ('origin', 'like', 'OUT/')]
-#        proc_ids = proc_obj.search(cr, uid, dom)
-#        for op in res_planned:
-#            for proc in proc_obj.browse(cr, uid, proc_ids, context=context):
-#                if proc.state in ('cancel', 'done'):
-#                    continue
-#                proc_qty = uom_obj._compute_qty_obj(cr, uid, proc.product_uom, proc.product_qty, proc.product_id.uom_id, context=context)
-#                res_planned[op] -= proc_qty
-#        return res_planned
 
 
 class procurement_order(models.Model):
@@ -163,8 +128,6 @@ class procurement_order(models.Model):
     # procurement/procurement:run_scheduler
     def run_procurement(self, cr, uid, ids, use_new_cursor=False, context=None):
         context = context or {}
-        import pdb
-        pdb.set_trace()
         try:
             if use_new_cursor:
                 cr = openerp.registry(cr.dbname).cursor()
@@ -203,51 +166,26 @@ class procurement_order(models.Model):
                     pass
         return {}
 
-    def _get_procurement_date_start(self, cr, uid, modelpoint, to_date, context=None):
-        #days = context.get('child_to_date', False) and 0.0 or self._get_bucket_delay(cr, uid, context=context)
-        #days = self._get_bucket_delay(cr, uid, context=context) or 0.0
+    def _get_procurement_date_start(self, cr, uid, orderpoint, to_date, context=None):
         days = 0.0
-        if modelpoint._name == 'stock.warehouse.orderpoint':
-            # make addition of lead_days an optional setting
-            days += modelpoint.lead_days or 0.0
-            modelpoint = modelpoint.product_id
-        #    if modelpoint.lead_type=='supplier':
-        #        days += modelpoint.product_id._select_seller(modelpoint.product_id).delay or 0.0
-        #    else:
-        #        days += modelpoint.product_id.produce_delay or 0.0
-        #        days += modelpoint.company_id.manufacturing_lead
-        #if modelpoint._name == 'product.product':
-        #    for route in modelpoint.route_ids:
-        #        if route.id == self.pool.get('ir.model.data').xmlid_to_res_id(cr, uid, 'purchase.route_warehouse0_buy'):
-        #            days += modelpoint._select_seller(modelpoint).delay or 0.0
-        #        if route.id == self.pool.get('ir.model.data').xmlid_to_res_id(cr, uid, 'mrp.route_warehouse0_manufacture'):
-        #            days += modelpoint.produce_delay or 0.0
-        #            days += modelpoint.product_tmpl_id.company_id.manufacturing_lead
-        for route in modelpoint.route_ids:
+        # make addition of lead_days an optional setting
+        days += orderpoint.lead_days or 0.0
+        product = orderpoint.product_id
+        for route in product.route_ids:
             if route.pull_ids:
                 for rule in route.pull_ids:
                     if rule.action == 'buy':
-                        days += modelpoint._select_seller(modelpoint).delay or 0.0
-                        days += modelpoint.product_tmpl_id.company_id.po_lead
+                        days += product._select_seller(product).delay or 0.0
+                        days += product.product_tmpl_id.company_id.po_lead
                     if rule.action == 'manufacture':
-                        days += modelpoint.produce_delay or 0.0
-                        days += modelpoint.product_tmpl_id.company_id.manufacturing_lead
+                        days += product.produce_delay or 0.0
+                        days += product.product_tmpl_id.company_id.manufacturing_lead
         date_start = datetime.combine(datetime.strptime(to_date, DEFAULT_SERVER_DATE_FORMAT) - relativedelta(days=days), datetime.min.time())
         return date_start.strftime(DEFAULT_SERVER_DATE_FORMAT)
-
-    # from 9.0, compare with considerations for _get_procurement_date_start
-    #def _get_orderpoint_date_planned(self, cr, uid, orderpoint, start_date, context=None):
-    #    days = orderpoint.lead_days or 0.0
-    #    if orderpoint.lead_type=='purchase':
-    #        # These days will be substracted when creating the PO
-    #        days += orderpoint.product_id._select_seller(orderpoint.product_id).delay or 0.0
-    #    date_planned = start_date + relativedelta(days=days)
-    #    return date_planned.strftime(DEFAULT_SERVER_DATE_FORMAT)
 
     # stock/procurement,mrp_time_bucket/mrp_time_bucket
     def _prepare_orderpoint_procurement(self, cr, uid, orderpoint, product_qty, context=None):
         res = super(procurement_order, self)._prepare_orderpoint_procurement(cr, uid, orderpoint, product_qty, context=context)
-        #res['date_start'] = self._get_procurement_date_start(cr, uid, orderpoint, context['to_date'], context=context)
         res['date_start'] = self._get_procurement_date_start(cr, uid, orderpoint, context['bucket_date'], context=context)
         _logger.info(" IN res: %s", res)
         return res
@@ -262,7 +200,6 @@ class procurement_order(models.Model):
         # it seems the location_id for an OB proc should be some form of production (dest), but no ops would exist for such
         #   perhaps should reconsider:
         #    'location_id': self.pool.get('ir.model.data').xmlid_to_res_id(cr, uid, 'stock.location_production'),
-        #child_date = self._get_procurement_date_planned(cr, uid, context['child_to_date'], context=context)
         all_parent_location_ids = self._find_parent_locations(cr, uid, orderpoint, context=context)
         location_domain = [('location_id', 'in', all_parent_location_ids)]
         child_orderpoint_id = self.pool.get('stock.warehouse.orderpoint').search(cr, uid, [
@@ -287,8 +224,6 @@ class procurement_order(models.Model):
         child_location_id = child_rule_id and self.pool.get('procurement.rule').browse(cr, uid, child_rule_id)[0].location_src_id or False
         res = {
             'name': product.product_tmpl_id.name,
-            #'date_planned': self._get_procurement_date_planned(cr, uid, context['child_to_date'], context=context),
-            #'date_planned': child_date,
             'date_planned': context['child_to_date'],
             'product_id': product.id,
             'product_qty': product_qty,
@@ -300,24 +235,24 @@ class procurement_order(models.Model):
             'origin': 'OUT/PROC/' + '%%0%sd' % 5 % context['parent_proc_id'],
             #'warehouse_id': loc_obj.get_warehouse(cr, uid, loc_obj.browse(cr, uid, location_id), context=context),
             'warehouse_id': orderpoint.warehouse_id.id,
-            #'date_start': self._get_procurement_date_start(cr, uid, product, context['child_to_date'], context=context),
-            # if _get_proc_date_start is not used here we can simplify that method to no longer need to check for product.product
-            #'date_start': child_date,
             'date_start': context['child_to_date'],
             'orderpoint_id': child_orderpoint_id and child_orderpoint_id[0] or False,
         }
         _logger.info("OUT res: %s", res)
         return res
 
-    # mrp_time_bucket/mrp_time_bucket
+    # override mrp_time_bucket/mrp_time_bucket
     def _process_procurement(self, cr, uid, ids, context=None):
         # REPLACE _process_procurement to eliminate automatic execution of run for confirmed procurements
-        self.check(cr, uid, ids)
+        #self.check(cr, uid, ids)
+        pass
 
     # mrp_time_bucket/mrp_time_bucket
     def _plan_orderpoint_procurement(self, cr, uid, op, qty_rounded, context=None):
         context = context or {}
         proc_id = super(procurement_order, self)._plan_orderpoint_procurement(cr, uid, op, qty_rounded, context=context)
+        proc_ids = []
+        proc_ids.append(proc_id)
         if proc_id:
             bom_obj = self.pool.get('mrp.bom')
             bom_id = bom_obj._bom_find(cr, uid, product_id=op.product_id.id, context=context)
@@ -332,24 +267,19 @@ class procurement_order(models.Model):
                 res = bom_obj._bom_explode(cr, uid, bom_point, proc_point.product_id, factor / bom_point.product_qty, context=context)
                 # product_lines
                 results = res[0]
-                # aa.scheduledate = a.sd - a.produce_delay
-                #bucket_delay = self._get_bucket_delay(cr, uid, context=context)
-                ## this 'new' to_date is used for bucket purposes so if weekly buckets in use, will need to line up with preceding bucket tail end
-                ##context['child_to_date'] = (datetime.strptime(context['to_date'],DEFAULT_SERVER_DATE_FORMAT) + relativedelta(days=bucket_delay) - relativedelta(days=proc_point.product_id.produce_delay)).strftime(DEFAULT_SERVER_DATE_FORMAT)
-                #days = proc_point.product_id.produce_delay + proc_point.product_id.product_tmpl_id.company_id.manufacturing_lead + bucket_delay
-                #context['child_to_date'] = (datetime.strptime(context['to_date'],DEFAULT_SERVER_DATE_FORMAT) - relativedelta(days=days)).strftime(DEFAULT_SERVER_DATE_FORMAT)
                 context['child_to_date'] = proc_point.date_start
                 context['parent_proc_id'] = proc_id
                 # process procurements for results
                 for product in results:
                     product_obj = self.pool.get('product.product')
                     product_point = product_obj.browse(cr, uid, product['product_id'])
-                    proc_id = procurement_obj.create(cr, uid,
+                    proc_id += procurement_obj.create(cr, uid,
                                                     self._prepare_outbound_procurement(cr, uid, op, product_point, product['product_qty'], product['product_uom'], context=context),
                                                     context=context)
-                #    self._process_procurement(cr, uid, proc_id, context=context)
+                    proc_id and proc_ids.append(proc_id) or False
                 context.pop('child_to_date')
                 context.pop('parent_proc_id')
+        return proc_ids
 
     # stock/procurement,mrp_time_bucket/mrp_time_bucket
     def _procure_orderpoint_confirm(self, cr, uid, use_new_cursor=False, company_id = False, context=None):
--- mrp_time_bucket/mrp_time_bucket.py
+++ mrp_time_bucket/mrp_time_bucket.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # See __openerp__.py file for full copyright and licensing details.
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from openerp import models
 from dateutil.relativedelta import relativedelta
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, \
@@ -27,46 +27,29 @@ class procurement_order(models.Model):
         #   the user to choose when product should be available and in addition
         #   to the time_bucket, subtract the relative # of days (MON thru SUN)
         #     e.g. MON = -6, - relativedelta(days=6)
-        #return 1
-        #return self._get_bucket_size(cr, uid, context=context)
         time_bucket = self._get_bucket_size(cr, uid, context=context)
         bucket_day = time_bucket == 7 and 1 or 0 # 1 or preset
         bucket_delay = time_bucket - bucket_day
         return bucket_delay
 
-    #def _get_procurement_date_planned(self, cr, uid, to_date, context=None):
-    #    days = self._get_bucket_delay(cr, uid, context=context) or 0.0
-    #    #days = 0.0
-    #    date_planned = datetime.combine(datetime.strptime(to_date, DEFAULT_SERVER_DATE_FORMAT) - relativedelta(days=days), datetime.min.time())
-    #    return date_planned.strftime(DEFAULT_SERVER_DATE_FORMAT)
-
     # stock/procurement
     def _prepare_orderpoint_procurement(self, cr, uid, orderpoint, product_qty, context=None):
         res = super(procurement_order, self)._prepare_orderpoint_procurement(cr, uid, orderpoint, product_qty, context=context)
-        #res['date_planned'] = self._get_procurement_date_planned(cr, uid, context['to_date'], context=context)
         res['date_planned'] = context['bucket_date']
         return res
 
+    # Method to override in mrp_procurement_only module
     def _process_procurement(self, cr, uid, ids, context=None):
-        self.check(cr, uid, ids)
+        #self.check(cr, uid, ids)
         self.run(cr, uid, ids)
 
-    # Method to overwrite in mrp_procurement_only module
+    # Method to override in mrp_procurement_only module
     def _plan_orderpoint_procurement(self, cr, uid, op, qty_rounded, context=None):
         procurement_obj = self.pool.get('procurement.order')
         proc_id = procurement_obj.create(cr, uid,
                                         self._prepare_orderpoint_procurement(cr, uid, op, qty_rounded, context=context),
                                         context=context)
-        # self._process_procurement(cr, uid, proc_id, context=context)
         return proc_id
-
-    #def _product_virtual_get(self, cr, uid, order_point, context=None):
-    #    context = context or {}
-    #    context['location'] = order_point.location_id.id
-    #    product_obj = self.pool.get('product.product')
-    #    return product_obj._product_available(cr, uid,
-    #            [order_point.product_id.id],
-    #            context=context)[order_point.product_id.id]['virtual_available']
 
     # stock/procurement
     def _procure_orderpoint_confirm(self, cr, uid, use_new_cursor=False, company_id = False, context=None):
@@ -89,27 +72,18 @@ class procurement_order(models.Model):
         #        scheduled date when using weekly bucket
         #      planning_horizon (first_bucket_dt) should start on a monday
         #        for weekly bucket
-        #time_bucket = 1
         time_bucket = self._get_bucket_size(cr, uid, context=context)
         # get bucket datetime objects
-        #first_bucket_dt = datetime.combine(datetime.utcnow().date(), datetime.min.time())i
         utc_dt = datetime.combine(datetime.utcnow().date(), datetime.min.time())
         first_bucket_dt = time_bucket == 7 and utc_dt - timedelta(days=utc_dt.weekday()) or utc_dt
         last_bucket_dt = utc_dt
-        #first_procurement = procurement_obj.search(cr, uid, [('state', '=', 'running')], order="date_planned ASC", limit=1)
-        #if first_procurement:
-        #    first_procurement_id = first_procurement[0]
-        ##first_procurement_id = procurement_obj.search(cr, uid, [('state', '=', 'running')], order="date_planned ASC", limit=1)[0]
-        #    first_bucket_dt = datetime.strptime(procurement_obj.browse(cr, uid, first_procurement_id)['date_planned'], DEFAULT_SERVER_DATETIME_FORMAT)
         last_procurement = procurement_obj.search(cr, uid, [('state', '=', 'running')], order="date_planned DESC", limit=1)
         if last_procurement:
             last_procurement_id = last_procurement[0]
-        #last_procurement_id = procurement_obj.search(cr, uid, [('state', '=', 'running')], order="date_planned DESC", limit=1)[0]
             last_bucket_dt = datetime.strptime(procurement_obj.browse(cr, uid, last_procurement_id)['date_planned'], DEFAULT_SERVER_DATETIME_FORMAT)
             last_bucket_dt = time_bucket == 7 and last_bucket_dt + timedelta(days=time_bucket - last_bucket_dt.isoweekday()) or last_bucket_dt
             last_bucket_dt = (last_bucket_dt < first_bucket_dt) and first_bucket_dt or last_bucket_dt
-        # get delta from first to last procurement 'date_planned'
-        #planning_horizon = (last_bucket_dt - first_bucket_dt).days + 1
+        # get delta from first to last bucket datetime objects
         planning_horizon = (last_bucket_dt - first_bucket_dt).days + time_bucket
 
         dom = company_id and [('company_id', '=', company_id)] or []
@@ -136,27 +110,19 @@ class procurement_order(models.Model):
             for key in sorted(product_dict.keys()):
                 plan_days = 0
                 while plan_days <= planning_horizon:
-                    # set context
                     to_date = first_bucket_dt.date() + relativedelta(days=plan_days)
-                    #to_date = first_bucket_dt.date() + relativedelta(days=plan_days - self._get_bucket_delay(cr, uid, context=context))
                     _logger.info("to_date: %s", to_date)
-                    #dict(context)['to_date'] = (from_date + relativedelta(days=time_bucket)).strftime(DEFAULT_SERVER_DATE_FORMAT)
 
                     ctx = context.copy()
                     ctx.update({'location': ops_dict[key][0].location_id.id})
-                    #ctx.update({'to_date': (from_date + relativedelta(days=time_bucket)).strftime(DEFAULT_SERVER_DATE_FORMAT)})
                     ctx.update({'bucket_date': (to_date - relativedelta(days=self._get_bucket_delay(cr, uid, context=context))).strftime(DEFAULT_SERVER_DATE_FORMAT)})
                     ctx.update({'to_date': to_date.strftime(DEFAULT_SERVER_DATE_FORMAT)})
                     ctx.update({'procurement_autorun_defer': True})
                     prod_qty = product_obj._product_available(cr, uid, [x.id for x in product_dict[key]],
                                                             context=ctx)
                     subtract_qty = orderpoint_obj.subtract_procurements_from_orderpoints(cr, uid, [x.id for x in ops_dict[key]], context=ctx)
-                    #if key==(12,1) and plan_days>2:
-                    #    import pdb
-                    #    pdb.set_trace()
                     for op in ops_dict[key]:
                         try:
-                            # prods = self._product_virtual_get(cr, uid, op, context=context)
                             prods = prod_qty[op.product_id.id]['virtual_available']
                             _logger.info("op: %s", op)
                             _logger.info("prods: %s", prods)
@@ -180,7 +146,7 @@ class procurement_order(models.Model):
                                 qty_rounded = float_round(qty, precision_rounding=op.product_uom.rounding)
                                 if qty_rounded > 0:
                                     proc_id = self._plan_orderpoint_procurement(cr, uid, op, qty_rounded, context=ctx)
-                                    tot_procs.append(proc_id)
+                                    tot_procs.extend(proc_id) if isinstance(proc_id, list) else tot_procs.append(proc_id)
                                 if use_new_cursor:
                                     cr.commit()
                         except OperationalError:
@@ -192,7 +158,7 @@ class procurement_order(models.Model):
                                 raise
                     plan_days = plan_days + time_bucket
             try:
-                # tot_procs.reverse()
+                tot_procs.reverse()
                 # self.run(cr, uid, tot_procs, context=context)
                 self._process_procurement(cr, uid, tot_procs, context=context)
                 tot_procs = []
