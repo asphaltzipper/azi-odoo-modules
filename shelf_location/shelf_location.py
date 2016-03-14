3020650e7ab0f3fe18f17318db17cc3ab51bb63f
matt454357 <matt454357@gmail.com>
2016-03-14 12:27:24 -0600

    shelf_location -bista changes per request from azi

--- shelf_location/shelf_location.py
+++ shelf_location/shelf_location.py
@@ -10,16 +10,42 @@ class ShelfLocation(models.Model):
         prod_ids = []
         product_obj = self.env["product.template"]
         tools_obj = self.env['tools.master']
-        related_recordset = product_obj.search([])
+        related_recordset = product_obj.search(['|',('active', '=', False),('active', '=', True)])
         for product_rec in related_recordset:
-            for loc_rec in product_rec.location_ids:
-                 if loc_rec.location_name.name == self.location_id.name:
+            for loc_rec in product_rec.shelf_ids:
+                if loc_rec.name == self.location_id.name:
                      prod_ids.append((4,product_rec.id))
         self.product_id = prod_ids
+
+    @api.depends('product_id')
+    def inactive_product_count(self):
+        prod_ids = []
+        product_obj = self.env["product.template"]
+        tools_obj = self.env['tools.master']
+        related_recordset = product_obj.search([('active', '=', False)])
+        for product_rec in related_recordset:
+            for loc_rec in product_rec.shelf_ids:
+                if loc_rec.name == self.location_id.name:
+                    prod_ids.append((4,product_rec.id))
+        self.inactives = len(prod_ids)
+
+    api.depends('product_id')
+    def total_product_count(self):
+        prod_ids = []
+        product_obj = self.env["product.template"]
+        tools_obj = self.env['tools.master']
+        related_recordset = product_obj.search(['|',('active', '=', False),('active', '=', True)])
+        for product_rec in related_recordset:
+            for loc_rec in product_rec.shelf_ids:
+                if loc_rec.name == self.location_id.name:
+                    prod_ids.append((4,product_rec.id))
+        self.product_count = len(prod_ids)
 
     name = fields.Char(string='Name')
     location_id = fields.Many2one('tools.master',string='Location')
     product_id = fields.One2many('product.template', 'product_id', string='Products', compute="location_product")
+    inactives = fields.Integer(string='Inactives', compute='inactive_product_count')
+    product_count = fields.Integer(string='Total Product', compute='total_product_count')
 
 class ToolsMaster(models.Model):
     _name = 'tools.master'
@@ -34,8 +60,6 @@ class ToolsDetail(models.Model):
 
     name = fields.Char(string='Location')
     location_name =  fields.Many2one('tools.master', string='Location')
-    item_assign = fields.Integer(string='Items Assigned')
-    inactives = fields.Integer(string='Inactives')
     product_id = fields.Many2one('product.template', string='Product')
 
 class ProductTemplate(models.Model):
@@ -43,3 +67,5 @@ class ProductTemplate(models.Model):
 
     location_ids = fields.One2many('tools.detail','product_id',string="Scannning Details")
     product_id = fields.Many2one('product.template', string='Product')
+    shelf_ids = fields.Many2many('tools.master', 'product_tmpl_id', 'shelf_id', string='Shelves')
+
