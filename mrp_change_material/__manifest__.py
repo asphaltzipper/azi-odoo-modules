5d4f3b8219f264156316daca9e6252e036e01da3
Matt Taylor <matt454357@gmail.com>
2017-07-12 17:50:29 -0600

    add the beginnings of mrp_change_material module

---
+++ mrp_change_material/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Matt Taylor
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "mrp_change_material",
+    "version": "10.0.0.1",
+    "summary": "MRP Change Material",
+    "category": "Manufacturing",
+    "author": "Matt Taylor",
+    "website": "http://www.asphaltzipper.com",
+    'description': """
+Change Manufacturing Order Consumed Material
+============================================
+
+* Add a cancel button to each Consumed Materials line
+* Enable creation of new Consumed Materials lines
+* Increase quantity by creating new lines
+* Decrease quantity by creating a new line and canceling the old line
+    """,
+    "depends": ['mrp'],
+    'data': [
+        'views/mrp_production_views.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+}
