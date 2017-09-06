73f29abe9e3d00ed9341c549f72b221ef5c64e76
Matt Taylor <matt454357@gmail.com>
2017-09-06 13:03:36 -0600

    hacking on partner_credit_limit: untested

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -11,22 +11,36 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.one
     def check_limit(self):
+        self.ensure_one()
         partner = self.partner_id
-        moveline_obj = self.env['account.move.line']
-        movelines = moveline_obj.\
-            search([('partner_id', '=', partner.id),
-                    ('account_id.user_type_id.name', 'in',
-                    ['Receivable', 'Payable']),
-                    ('full_reconcile_id', '=', False)])
 
+        # sum unreconciled debits and credits from journal items
+        domain = [('partner_id', '=', partner.id),
+                  ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
+                  ('full_reconcile_id', '=', False)]
+        move_lines = self.env['account.move.line'].search(domain)
         debit, credit = 0.0, 0.0
-        today_dt = datetime.strftime(datetime.now().date(), DF)
-        for line in movelines:
+        today_dt = datetime.today().strftime(DF)
+        for line in move_lines:
             if line.date_maturity < today_dt:
                 credit += line.debit
-                debit += line.credit
+
+        # sum sale order lines that are not yet invoiced, from all the sale orders that are confirmed
+        domain = [('order_id', '!=', self.id),
+                  ('order_id.partner_id', '=', self.partner_id.id),
+                  ('invoice_status', '=', 'to invoice'),
+                  ('order_id.state', 'not in', ['draft', 'cancel', 'sent'])]
+        order_lines = self.env['sale.order.line'].search(domain)
+        not_invoiced_amount = sum([x.price_subtotal for x in order_lines])
+
+        # sum the total amount of all the invoices that are in draft state
+        domain = [('partner_id', '=', self.partner_id.id),
+                  ('state', '=', 'draft')]
+        draft_invoices = self.env['account.invoice'].search(domain)
+        draft_invoices_amount = sum([x.amount_total for x in draft_invoices])
+
+        debit += line.credit
 
         if (credit - debit + self.amount_total) > partner.credit_limit:
             if not partner.over_credit:
@@ -40,6 +54,44 @@ class SaleOrder(models.Model):
                 return True
         else:
             return True
+
+    @api.one
+    def check_limit(self):
+        if self.payment_term_id != self.env.ref('account.account_payment_term_immediate'):
+            if self.partner_id.warning_type != 'none':
+                if self.partner_id.warning_type in ('date', 'all'):
+                    d = datetime.timedelta(days=self.partner_id.credit_limit_days)
+                    if self.partner_id.payment_earliest_due_date == False:
+                        return True
+                    data = self.partner_id.payment_earliest_due_date
+                    if data + d < datetime.now():
+                        msg = 'Can not confirm the order because the customer does not have enough credit. \
+                            You can transition your billing policy to direct debit to be able to bill."'
+                        raise Warning(_(msg))
+                        return False
+                if self.order_id and self.order_id.partner_id.warning_type in ('value','all'):
+                    # We sum from all the sale orders that are approved, the sale order
+                    # lines that are not yet invoiced
+                    domain = [('order_id.partner_id', '=', self.partner_id.id),
+                              ('invoice_status', '=', 'to invoice'),
+                              ('order_id.state', 'not in', ['draft', 'cancel', 'sent'])]
+                    order_lines = self.env['sale.order.line'].search(domain)
+                    none_invoiced_amount = sum([x.price_subtotal for x in order_lines])
+                    # We sum from all the invoices that are in draft the total amount
+                    domain = [
+                        ('partner_id', '=', self.partner_id.id), ('state', '=', 'draft')]
+                    draft_invoices = self.env['account.invoice'].search(domain)
+                    draft_invoices_amount = sum([x.amount_total for x in draft_invoices])
+
+                    available_credit = self.partner_id.credit_limit - \
+                        self.partner_id.credit - \
+                        none_invoiced_amount - draft_invoices_amount
+                    if self.amount_total > available_credit:
+                        msg = 'Can not confirm the order because the customer does not have enough credit. \
+                            You can transition your billing policy to direct debit to be able to bill."'
+                        raise Warning(_(msg))
+                        return False
+        return True
 
     @api.multi
     def action_confirm(self):
