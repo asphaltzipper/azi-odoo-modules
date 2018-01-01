db8551f16dcccd42ad99ff68ba8bda1d7b527a8a
Matt Taylor <matt454357@gmail.com>
2018-01-01 16:23:24 -0700

    fix serial owners required fields

--- serial_crm/models/stock_lot_partner.py
+++ serial_crm/models/stock_lot_partner.py
@@ -13,9 +13,11 @@ class StockLotPartner(models.Model):
 
     lot_id = fields.Many2one(
         comodel_name='stock.production.lot',
-        string='Serial')
+        string='Serial',
+        required=True)
 
     partner_id = fields.Many2one(
         comodel_name='res.partner',
         string='Partner',
+        required='True',
         domain=[('customer', '=', True)])
