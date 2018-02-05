628eb969d5e5858f1f898ee3aab5540050dbd54b
Chris Emigh <cemigh@asphaltzipper.com>
2018-02-05 15:29:08 -0700

    comment the manifest

--- web_freeze_tree_header/__manifest__.py
+++ web_freeze_tree_header/__manifest__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# https://github.com/maxodoo/max-misc-addons/tree/10.0/max_web_freeze_list_view_header
+# commit: 58ad650139e69e8dc76fc92c0cd318da49210471
+
 {
     'name': "Web Freeze List View Header",
 
