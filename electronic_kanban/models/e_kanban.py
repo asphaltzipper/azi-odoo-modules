7bdfa432900a416cca534b3154b87b4c1b0637eb
Chris Emigh <cemigh@asphaltzipper.com>
2018-03-16 17:14:37 -0600

    Fix deleted default field

--- electronic_kanban/models/e_kanban.py
+++ electronic_kanban/models/e_kanban.py
@@ -93,7 +93,6 @@ class EKanbanBatchLine(models.Model):
         selection=[],
         string='Product Type',
         related='product_id.product_tmpl_id.type',
-        default='consu',
         required=True)
 
     e_kanban = fields.Boolean(
