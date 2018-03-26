b2140535b87be5e8b20f7bdc482e03b196b07023
Matt Taylor <matt454357@gmail.com>
2018-03-26 14:20:31 -0600

    add stock_internal_transport module

---
+++ stock_internal_transport/models/stock_picking.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    transport = fields.Boolean(string="Transportation")
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    type_transport = fields.Boolean(
+        string='Transport',
+        related='picking_type_id.transport',
+        readonly=True)
+
+    srce_contact_id = fields.Many2one(
+        comodel_name='res.partner',
+        string="Source Address")
+
+    dest_contact_id = fields.Many2one(
+        comodel_name='res.partner',
+        string="Destination Address")
+
+    @api.onchange('location_id')
+    def onchange_location_id(self):
+        if not self.type_transport:
+            return
+        if self.location_id.partner_id:
+            self.srce_contact_id = self.location_id.partner_id
+
+    @api.onchange('location_dest_id')
+    def onchange_location_dest_id(self):
+        if not self.type_transport:
+            return
+        if self.location_dest_id.partner_id:
+            self.dest_contact_id = self.location_dest_id.partner_id
