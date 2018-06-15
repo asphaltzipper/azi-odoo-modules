00497bd05a888e17cf77c54ef413d0a91797490d
Matt Taylor <matt454357@gmail.com>
2018-06-14 19:50:13 -0600

    fix bug in bom sequence sorting
    the sequence field had null values
    null values sort after not null integers
    the interface showed a zero, even though the field was null
    that made it appear that zeros were sorting last
    we fix it by setting a default value of 1 for the sequence field
    
    this fix should be accompanied with a database update that sets all nulls or zeros to one

--- azi_mrp/models/mrp_bom.py
+++ azi_mrp/models/mrp_bom.py
@@ -7,7 +7,7 @@ class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
     # require product variant on every bom
-    # isn't there a way to do this without overriding the entire ?
+    # isn't there a way to do this without overriding the entire definition?
     # product_id = fields.Many2one(required=True)
     product_id = fields.Many2one(
         comodel_name='product.product',
@@ -15,3 +15,9 @@ class MrpBom(models.Model):
         required=True,
         domain="['&', ('product_tmpl_id', '=', product_tmpl_id), ('type', 'in', ['product', 'consu'])]",
         help="If a product variant is defined the BOM is available only for this product.")
+
+    # null sorts last??!!
+    # the default is null??!!!
+    # I thought integer fields could not be null?
+    # okay, make the default one
+    sequence = fields.Integer(default=1)
