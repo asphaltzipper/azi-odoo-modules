f66b5702994b81fda0d34731887d59d7432e8bd4
John Welch <jwelch@gmail.com>
2020-05-06 10:03:20 -0600

    Create a new report for printing kanban

---
+++ azi_stock_request_kanban/README.rst
@@ -0,0 +1,15 @@
+AZI Stock Request Kanban
+========================
+* Create new report "AZI Print Kanban
+
+Installation
+============
+* No specific installation required.
+
+Configuration
+=============
+* No specific configuration required.
+
+Usage
+=====
+* AZI Print Kanban Report: Stock Requests > Master Data > Kanban Cards
---
+++ azi_stock_request_kanban/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'license': 'LGPL-3',
+    "name": "AZI Stock Request Kanban",
+    "version": "12.0.1.1.0",
+    "author": "Asphalt Zipper",
+    "category": "Extra Tools",
+    "summary": "Modify Kanban Report",
+    'description': """
+    This module modify kanban report.""",
+    "website": "http://www.github.com/asphaltzipper",
+    "depends": ["stock_request_kanban", "azi_stock"],
+    "data": [
+        'report/stock_request_kanban_template.xml',
+    ],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
+
