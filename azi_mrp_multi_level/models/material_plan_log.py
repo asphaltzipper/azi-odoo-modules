6e45d06775aab5576beccb4c14cf22afa03ed440
Matt Taylor <matt454357@gmail.com>
2020-07-17 13:07:18 -0600

    improve logging on mrp_multi_level

--- azi_mrp_multi_level/models/material_plan_log.py
+++ azi_mrp_multi_level/models/material_plan_log.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class MaterialPlanLog(models.Model):
     _name = 'material.plan.log'
     _description = 'Plan Material Log'
+    _order = 'create_date desc'
 
     type = fields.Selection([('info', 'Info'), ('debug', 'Debug'), ('warning', 'Warning')], 'Type', required=True,
                             readonly=True)
