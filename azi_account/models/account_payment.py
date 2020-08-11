4ca0cc1c4feb8d9f19c2f362b290bf539d0bbada
Matt Taylor <matt454357@gmail.com>
2020-08-11 12:40:59 -0600

    fix check number in account move ref field

--- azi_account/models/account_payment.py
+++ azi_account/models/account_payment.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models
+from odoo import models, api
 
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    def _get_move_vals(self, journal=None):
-        """ Return dict to create the payment move
-        """
-        res = super(AccountPayment, self)._get_move_vals(journal)
-        if self.check_number:
-            res['ref'] = "CHECK %d: %s" % (self.check_number, res['ref'])
+    @api.multi
+    def post(self):
+        res = super(AccountPayment, self).post()
+        payment_method_check = self.env.ref('account_check_printing.account_payment_method_check')
+        for payment in self.filtered(lambda p: p.payment_method_id == payment_method_check and p.check_manual_sequencing):
+            for entry in payment.move_line_ids.mapped('move_id'):
+                if not entry.ref.startswith("CHECK "):
+                    entry.ref = "CHECK %s: %s" % (str(payment.check_number), entry.ref)
         return res
