4d82964e900385cda033ad156acd8ad318a10e99
Matt Taylor <matt454357@gmail.com>
2021-01-08 17:09:25 -0700

    reduce report load-time by 95 percent

--- azi_stock_inventory_turnover_report/models/product.py
+++ azi_stock_inventory_turnover_report/models/product.py
@@ -6,16 +6,22 @@ from odoo import fields, models
 class Product(models.Model):
     _inherit = "product.product"
 
-    average_cost_6m = fields.Float(compute="_compute_inventory_turn_report",
-                                   help="Average cost of inventory in the last 6 months")
-    average_cost_12m = fields.Float(compute="_compute_inventory_turn_report",
-                                    help="Average cost of inventory in the last 12 months")
-    consumed_cost_6m = fields.Float(compute="_compute_inventory_turn_report",
-                                    help="Consumed cost in the last 6 months")
-    consumed_cost_12m = fields.Float(compute="_compute_inventory_turn_report",
-                                     help="Consumed cost in the last 12 months")
+    average_cost_6m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Average cost of inventory in the last 6 months")
+    average_cost_12m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Average cost of inventory in the last 12 months")
+    consumed_cost_6m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Consumed cost in the last 6 months")
+    consumed_cost_12m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Consumed cost in the last 12 months")
 
-    def get_average_cost_and_turns(self, product_id):
+    def get_average_cost_and_turns(self, product_ids):
+        if not product_ids:
+            return {}
         query = """
             with a as (
                 select
@@ -28,21 +34,14 @@ class Product(models.Model):
                 left join ir_property as ip on ip.res_id='product.category,'||pt.categ_id and ip.name='property_stock_valuation_account_id'
                 left join account_account as aa on 'account.account,'||aa.id=ip.value_reference
                 where pt.type='product'
-                and pt.active=true and pp.id = %s
+                and pt.active=true
             )
             select
                 a.product_id,
-                a.category_id,
                 (coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 as avg_6_balance,
                 (coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 as avg_12_balance,
                 coalesce(c6.credit, 0.0) as consumed_6_cost,
-                coalesce(c12.credit, 0.0) as consumed_12_cost,
-                case when ( (coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 ) <> 0.0 then
-                    coalesce(c6.credit, 0.0) / ( (coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 )
-                    else 0.0 end as turns_6,
-                case when ( (coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 ) <> 0.0 then
-                    coalesce(c12.credit, 0.0) / ( (coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 )
-                    else 0.0 end as turns_12
+                coalesce(c12.credit, 0.0) as consumed_12_cost
             from a
             left join (
                 select
@@ -88,13 +87,25 @@ class Product(models.Model):
                 where date >= now() - interval '12 months'
                 group by a.product_id
             ) as c12 on c12.product_id=a.product_id
+            where a.product_id in %s
         """
-        self.env.cr.execute(query, [str(product_id)])
-        _, _, avg_cost_6m, avg_cost_12m, consumed_cost_6m, consumed_cost_12m, inventory_turns_6m, \
-            inventory_turns_12_m = self.env.cr.fetchall()[0]
-        return avg_cost_6m, avg_cost_12m, consumed_cost_6m, consumed_cost_12m, inventory_turns_6m, inventory_turns_12_m
+        self.env.cr.execute(query, (tuple(product_ids),))
+        prod_turns = {}
+        for prod in self.env.cr.fetchall():
+            (prod_id, avg_6_bal, avg_12_bal, cons_6m, cons_12m) = prod
+            prod_turns[prod_id] = {
+                'avg_cost_6m': avg_6_bal,
+                'avg_cost_12m': avg_12_bal,
+                'consumed_cost_6m': cons_6m,
+                'consumed_cost_12m': cons_12m,
+                'inventory_turns_6m': avg_6_bal and cons_6m / avg_6_bal or 0.0,
+                'inventory_turns_12_m': avg_12_bal and cons_12m / avg_12_bal or 0.0,
+            }
+        return prod_turns
 
     def _compute_inventory_turn_report(self):
+        prod_turns = self.get_average_cost_and_turns(self.ids)
+
         month = relativedelta(months=1)
         today = fields.Date.today()
         at_6m_ago = today - 6 * month
@@ -108,12 +119,7 @@ class Product(models.Model):
         gotten6m = self._compute_quantities_in_dict(from_date=at_6m_ago)
         gotten12m = self._compute_quantities_in_dict(from_date=at_12m_ago)
         for prod in self:
-            avg_cost_6m, avg_cost_12m, consumed_cost_6m, consumed_cost_12m, inventory_turns_6m, \
-                inventory_turns_12_m = self.get_average_cost_and_turns(prod.id)
             prod.total_cost = prod.qty_available * prod.standard_price
-            # average inventory balance
-            prod.average_cost_6m = avg_cost_6m
-            prod.average_cost_12m = avg_cost_12m
             # Quantity initially available
             prod.qty_available_6m = available6m.get(prod.id)
             prod.qty_available_12m = available12m.get(prod.id)
@@ -136,31 +142,44 @@ class Product(models.Model):
             prod.months_of_inventory_12m = (
                 0.0 if not prod.qty_consumed_12m else
                 prod.qty_available / prod.qty_consumed_12m * 12)
+
+            turns = prod_turns.get(prod.id)
+            # average inventory balance
+            prod.average_cost_6m = turns and turns['avg_cost_6m']
+            prod.average_cost_12m = turns and turns['avg_cost_12m']
             # Consumed Cost
-            prod.consumed_cost_6m = consumed_cost_6m
-            prod.consumed_cost_12m = consumed_cost_12m
+            prod.consumed_cost_6m = turns and turns['consumed_cost_6m']
+            prod.consumed_cost_12m = turns and turns['consumed_cost_12m']
             # Turns / Cycles per Month
-            prod.inventory_turns_6m = inventory_turns_6m
-            prod.inventory_turns_12m = inventory_turns_12_m
+            prod.inventory_turns_6m = turns and turns['inventory_turns_6m']
+            prod.inventory_turns_12m = turns and turns['inventory_turns_12_m']
 
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
-    average_cost_6m = fields.Float(compute="_compute_inventory_turn_report",
-                                   help="Average cost of inventory in the last 6 months")
-    average_cost_12m = fields.Float(compute="_compute_inventory_turn_report",
-                                    help="Average cost of inventory in the last 12 months")
-    consumed_cost_6m = fields.Float(compute="_compute_inventory_turn_report",
-                                    help="Consumed cost in the last 6 months")
-    consumed_cost_12m = fields.Float(compute="_compute_inventory_turn_report",
-                                     help="Consumed cost in the last 12 months")
-    inventory_turns_6m = fields.Float(compute="_compute_inventory_turn_report",
-                                      help="Inventory Turns / Cycles in the last 6 months")
-    inventory_turns_12m = fields.Float(compute="_compute_inventory_turn_report",
-                                       help="Inventory Turns / Cycles in the last 12 months")
+    average_cost_6m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Average cost of inventory in the last 6 months")
+    average_cost_12m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Average cost of inventory in the last 12 months")
+    consumed_cost_6m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Consumed cost in the last 6 months")
+    consumed_cost_12m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Consumed cost in the last 12 months")
+    inventory_turns_6m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Inventory Turns / Cycles in the last 6 months")
+    inventory_turns_12m = fields.Float(
+        compute="_compute_inventory_turn_report",
+        help="Inventory Turns / Cycles in the last 12 months")
 
-    def get_average_cost_and_turns(self, category_id):
+    def get_average_cost_and_turns(self, cat_ids):
+        if not cat_ids:
+            return {}
         query = """
             with a as (
                 select
@@ -170,24 +189,20 @@ class ProductCategory(models.Model):
                 from product_product pp
                 left join product_template pt on pt.id=pp.product_tmpl_id
                 left join product_category as pc on pc.id=pt.categ_id
-                left join ir_property as ip on ip.res_id='product.category,'||pt.categ_id and ip.name='property_stock_valuation_account_id'
-                left join account_account as aa on 'account.account,'||aa.id=ip.value_reference
+                left join ir_property as ip
+                    on ip.res_id='product.category,'||pt.categ_id
+                    and ip.name='property_stock_valuation_account_id'
+                left join account_account as aa
+                    on 'account.account,'||aa.id=ip.value_reference
                 where pt.type='product'
-                and pt.active=true and pc.id = %s
+                and pt.active=true
             )
             select
-                a.product_id,
                 a.category_id,
-                (coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 as avg_6_balance,
-                (coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 as avg_12_balance,
-                coalesce(c6.credit, 0.0) as consumed_6_cost,
-                coalesce(c12.credit, 0.0) as consumed_12_cost,
-                case when ( (coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 ) <> 0.0 then
-                    coalesce(c6.credit, 0.0) / ( (coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 )
-                    else 0.0 end as turns_6,
-                case when ( (coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 ) <> 0.0 then
-                    coalesce(c12.credit, 0.0) / ( (coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2 )
-                    else 0.0 end as turns_12
+                sum((coalesce(b6.balance, 0.0) + coalesce(e.balance, 0.0)) / 2) as avg_6_balance,
+                sum((coalesce(b12.balance, 0.0) + coalesce(e.balance, 0.0)) / 2) as avg_12_balance,
+                sum(coalesce(c6.credit, 0.0)) as consumed_6_cost,
+                sum(coalesce(c12.credit, 0.0)) as consumed_12_cost
             from a
             left join (
                 select
@@ -233,29 +248,34 @@ class ProductCategory(models.Model):
                 where date >= now() - interval '12 months'
                 group by a.product_id
             ) as c12 on c12.product_id=a.product_id
+            where a.category_id in %s
+            group by a.category_id
         """
-        self.env.cr.execute(query, [str(category_id)])
-        avg_cost_6m = avg_cost_12m = consumed_cost_6m = consumed_cost_12m = inventory_turns_6m = \
-            inventory_turns_12_m = 0
+        self.env.cr.execute(query, (tuple(cat_ids),))
+        cat_turns = {}
         for turn_values in self.env.cr.fetchall():
-            avg_cost_6m += turn_values[2]
-            avg_cost_12m += turn_values[3]
-            consumed_cost_6m += turn_values[4]
-            consumed_cost_12m += turn_values[5]
-            inventory_turns_6m += turn_values[6]
-            inventory_turns_12_m += turn_values[7]
-        return avg_cost_6m, avg_cost_12m, consumed_cost_6m, consumed_cost_12m, inventory_turns_6m, inventory_turns_12_m
+            (cat_id, avg_6_bal, avg_12_bal, cons_6m, cons_12m) = turn_values
+            cat_turns[cat_id] = {
+                'avg_cost_6m': avg_6_bal,
+                'avg_cost_12m': avg_12_bal,
+                'consumed_cost_6m': cons_6m,
+                'consumed_cost_12m': cons_12m,
+                'inventory_turns_6m': avg_6_bal and cons_6m/avg_6_bal or 0.0,
+                'inventory_turns_12_m': avg_12_bal and cons_12m/avg_12_bal or 0.0,
+            }
+        return cat_turns
 
     def _compute_inventory_turn_report(self):
+        cat_turns = self.get_average_cost_and_turns(self.ids)
         for category in self:
-            avg_cost_6m, avg_cost_12m, consumed_cost_6m, consumed_cost_12m, inventory_turns_6m, \
-                inventory_turns_12_m = self.get_average_cost_and_turns(category.id)
+            if not cat_turns.get(category.id):
+                continue
             # average inventory balance
-            category.average_cost_6m = avg_cost_6m
-            category.average_cost_12m = avg_cost_12m
+            category.average_cost_6m = cat_turns[category.id]['avg_cost_6m']
+            category.average_cost_12m = cat_turns[category.id]['avg_cost_12m']
             # Consumed Cost
-            category.consumed_cost_6m = consumed_cost_6m
-            category.consumed_cost_12m = consumed_cost_12m
+            category.consumed_cost_6m = cat_turns[category.id]['consumed_cost_6m']
+            category.consumed_cost_12m = cat_turns[category.id]['consumed_cost_12m']
             # Turns / Cycles per Month
-            category.inventory_turns_6m = inventory_turns_6m
-            category.inventory_turns_12m = inventory_turns_12_m
+            category.inventory_turns_6m = cat_turns[category.id]['inventory_turns_6m']
+            category.inventory_turns_12m = cat_turns[category.id]['inventory_turns_12_m']
