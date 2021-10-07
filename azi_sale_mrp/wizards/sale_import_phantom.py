29729fa6dde5a0f6a20600bc8309596a52901505
John Welch <jwelch@gmail.com>
2021-10-07 11:02:46 -0600

    Modified import phantom wizard to import child

--- azi_sale_mrp/wizards/sale_import_phantom.py
+++ azi_sale_mrp/wizards/sale_import_phantom.py
@@ -14,10 +14,22 @@ class SaleImportPhantom(models.TransientModel):
         if self.quantity <= 0:
             raise ValidationError(_('Quantity should be greater than zero'))
 
+    def create_phantom_childs(self, child_line_ids, so_id):
+        for child in child_line_ids:
+            if child.child_bom_id.type == 'phantom':
+                self.create_phantom_childs(child.child_line_ids, so_id)
+            else:
+                self.env['sale.order.line'].create({'order_id': so_id, 'product_id': child.product_id.id,
+                                                    'product_uom_qty': child.product_qty * self.quantity,
+                                                    'discount_reason_id': self.discount_reason_id.id})
+
     def import_phantom_lines(self):
         so_id = self._context.get('active_id', False)
         if so_id:
             for bom_line in self.bom_id.bom_line_ids:
-                self.env['sale.order.line'].create({'order_id': so_id, 'product_id': bom_line.product_id.id,
-                                                    'product_uom_qty': bom_line.product_qty * self.quantity,
-                                                    'discount_reason_id': self.discount_reason_id.id})
+                if bom_line.child_bom_id.type == 'phantom':
+                    self.create_phantom_childs(bom_line.child_line_ids, so_id)
+                else:
+                    self.env['sale.order.line'].create({'order_id': so_id, 'product_id': bom_line.product_id.id,
+                                                        'product_uom_qty': bom_line.product_qty * self.quantity,
+                                                        'discount_reason_id': self.discount_reason_id.id})
