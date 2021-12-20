90e3cd51708c7fd1e0fe986a801a8d41de789b7c
Matt Taylor <matt454357@gmail.com>
2021-12-20 11:04:53 -0700

    improve kanban inventory
    - sort 2x1 label report by part number
    - show products added to the inventory, but having no kanbans

--- electronic_kanban/models/stock_inventory_kanban.py
+++ electronic_kanban/models/stock_inventory_kanban.py
@@ -4,6 +4,31 @@ from odoo import api, models, fields
 class StockInventoryKanban(models.Model):
     _inherit = 'stock.inventory.kanban'
 
+    product_ids = fields.Many2many(
+        'product.product', string='Products',
+        domain=[('type', 'in', ['product', 'consu'])],
+        ondelete='cascade',
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    unconfigured_product_ids = fields.Many2many(
+        comodel_name='product.product',
+        readonly=True,
+        compute='_compute_unconfigured_product'
+    )
+    count_unconfigured_products = fields.Integer(
+        string='Un-Configured Products',
+        readonly=True,
+        compute='_compute_unconfigured_product',
+    )
+
+    @api.depends('product_ids')
+    def _compute_unconfigured_product(self):
+        for rec in self:
+            rec.unconfigured_product_ids = rec.product_ids.filtered(
+                lambda r: not r.e_kanban_verified
+            )
+            rec.count_unconfigured_products = len(rec.unconfigured_product_ids)
+
     @api.multi
     def print_missing_kanbans_2x1(self):
         """ Print the missing kanban cards in order to restore them
