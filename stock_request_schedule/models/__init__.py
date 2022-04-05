f5ace63ca348bf7b32118637617a01ef9d83c674
John Welch <jwelch@gmail.com>
2022-04-05 10:22:33 -0600

    Cancelled stock request when cancelling MO

--- stock_request_schedule/models/__init__.py
+++ stock_request_schedule/models/__init__.py
@@ -1,3 +1,4 @@
 from . import product
 from . import stock_request
 from . import sale_order
+from . import mrp_production
---
+++ stock_request_schedule/models/mrp_production.py
@@ -0,0 +1,13 @@
+from odoo import models, api
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    @api.multi
+    def action_cancel(self):
+        res = super(MrpProduction, self).action_cancel()
+        for production in self:
+            if production.stock_request_ids:
+                production.stock_request_ids.action_cancel()
+        return res
