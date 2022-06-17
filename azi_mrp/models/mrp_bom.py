5479287d6c0cf54e8419ed423ea5781bb084ee59
Matt Taylor <matt454357@gmail.com>
2022-06-17 09:42:19 -0600

    fix bug in rpc_explode

--- azi_mrp/models/mrp_bom.py
+++ azi_mrp/models/mrp_bom.py
@@ -19,8 +19,24 @@ class MrpBom(models.Model):
         else:
             picking_type = False
         boms, lines = self.explode(product, quantity, picking_type)
-        boms_done = [(x[0].id, x[1]) for x in boms]
-        lines_done = [(x[0].id, x[1]) for x in lines]
+        boms_done = []
+        for bom in boms:
+            bom_row = (bom[0].id, {
+                'product_id': bom[1]['product'].id,
+                'parent_line_id': bom[1]['parent_line'] and bom[1]['parent_line'].id or False,
+                'qty': bom[1]['qty'],
+                'original_qty': bom[1]['original_qty'],
+            })
+            boms_done.append(bom_row)
+        lines_done = []
+        for line in lines:
+            line_row = (line[0].id, {
+                'product_id': line[1]['product'].id,
+                'parent_line_id': line[1]['parent_line'] and line[1]['parent_line'].id or False,
+                'qty': line[1]['qty'],
+                'original_qty': line[1]['original_qty'],
+            })
+            lines_done.append(line_row)
         return boms_done, lines_done
 
 
