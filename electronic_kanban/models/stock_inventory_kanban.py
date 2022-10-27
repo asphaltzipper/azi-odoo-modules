aac740b0f8b726569f198af3d6c38c22bfb5425f
Matt Taylor <matt454357@gmail.com>
2022-10-27 15:14:22 -0600

    add 24x70mm e-kanban label

--- electronic_kanban/models/stock_inventory_kanban.py
+++ electronic_kanban/models/stock_inventory_kanban.py
@@ -38,3 +38,13 @@ class StockInventoryKanban(models.Model):
             'azi_stock_request_kanban.action_report_kanban_label_2x1').report_action(
             self.missing_kanban_ids
         )
+
+    @api.multi
+    def print_missing_kanbans_24x70(self):
+        """ Print the missing kanban cards in order to restore them
+        """
+        self.ensure_one()
+        return self.env.ref(
+            'azi_stock_request_kanban.report_kanban_label_24x70').report_action(
+            self.missing_kanban_ids
+        )
