c8918c06ab284a7292279e4f0161a887ed05c5f5
John Welch <jwelch@gmail.com>
2023-02-28 09:23:51 -0700

    Redirected the procure wizard to the MOs created

---
+++ azi_mrp_multi_level/wizards/mrp_inventory_procure.py
@@ -0,0 +1,44 @@
+from odoo import api, models, tools, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class MrpInventoryProcure(models.TransientModel):
+    _inherit = 'mrp.inventory.procure'
+
+    @api.multi
+    def make_procurement(self):
+        self.ensure_one()
+        errors = []
+        for item in self.item_ids:
+            if not item.qty:
+                raise ValidationError(_("Quantity must be positive."))
+            values = item._prepare_procurement_values()
+            # Run procurement
+            try:
+                # Add context
+                self.env['procurement.group'].with_context(mrp_procure=True).run(
+                    item.product_id,
+                    item.qty,
+                    item.uom_id,
+                    item.location_id,
+                    'INT: ' + str(self.env.user.login),  # name?
+                    'INT: ' + str(self.env.user.login),  # origin?
+                    values
+                )
+                item.planned_order_id.qty_released += item.qty
+            except UserError as error:
+                errors.append(error.name)
+            if errors:
+                raise UserError('\n'.join(errors))
+        mrp_orders = self.item_ids.mapped('planned_order_id.mrp_production_ids')
+        if mrp_orders:
+            return{
+                'type': 'ir.actions.act_window',
+                'name': _('Manufacturing Orders'),
+                'res_model': 'mrp.production',
+                'target': 'current',
+                'view_type': 'form',
+                'view_mode': 'tree,form',
+                'domain': [('id', 'in', mrp_orders.ids)]
+            }
+        return {'type': 'ir.actions.act_window_close'}
