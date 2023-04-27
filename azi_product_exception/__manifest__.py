135248bfdc6046f6264823a43f3c3a591cafcecd
John Welch <jwelch@gmail.com>
2023-04-27 10:46:07 -0600

    Migrated azi_product_exception from V 12.0 to V 16.0

--- azi_product_exception/__manifest__.py
+++ azi_product_exception/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     "name": "AZI Product Exception",
-    "version": "12.0.1.0.0",
+    "version": "1.0",
     "summary": "Receipt Warning",
     "category": "Warehouse",
     "author": "John Welch",
+    "license": "AGPL-3",
     "website": "http://www.github.com/asphaltzipper",
     'description': """
 AZI Product Exception
--- azi_product_exception/models/stock_move.py
+++ azi_product_exception/models/stock_move.py
@@ -9,8 +9,6 @@ class StockMove(models.Model):
         if not self.product_id:
             return
         warning = {}
-        title = False
-        message = False
         product_info = self.product_id
         if self.picking_id.picking_type_code == 'outgoing' and product_info.receipt_line_warn != 'no-message':
             title = _("Warning for %s") % product_info.name
@@ -19,5 +17,6 @@ class StockMove(models.Model):
             warning['message'] = message
             if product_info.receipt_line_warn == 'block':
                 self.product_id = False
+                self.product_uom = False
             return {'warning': warning}
         return {}
