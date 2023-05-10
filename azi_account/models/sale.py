0d6889517feb45bdbe8e8432041e3f75062a4d1f
John Welch <jwelch@gmail.com>
2023-05-10 12:40:02 -0600

    Fixed the singleton issue

--- azi_account/models/sale.py
+++ azi_account/models/sale.py
@@ -7,5 +7,5 @@ class SaleOrder(models.Model):
     def _finalize_invoices(self, invoices, references):
         super(SaleOrder, self)._finalize_invoices(invoices, references)
         for invoice in invoices.values():
-            if self.invoice_status == 'invoiced':
+            if self.filtered(lambda s: s.invoice_status == 'invoiced'):
                 invoice._onchange_invoice_line_ids()
