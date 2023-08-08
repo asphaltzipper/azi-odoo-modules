3c6dcfa3ce4653d5ee7b92e924b2de0f3dd00ab8
John Welch <jwelch@gmail.com>
2023-08-08 10:45:06 -0600

    Fixed the override of _query

--- sale_discount_reason/report/sale_report.py
+++ sale_discount_reason/report/sale_report.py
@@ -9,7 +9,12 @@ class SaleReport(models.Model):
         string='Discount Reason',
         readonly=True)
 
-    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        fields['discount_reason_id'] = ", l.discount_reason_id as discount_reason_id"
-        groupby += ', l.discount_reason_id'
-        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
+    def _select_additional_fields(self):
+        res = super(SaleReport, self)._select_additional_fields()
+        res['discount_reason_id'] = "l.discount_reason_id"
+        return res
+
+    def _group_by_sale(self):
+        res = super(SaleReport, self)._group_by_sale()
+        res += ', l.discount_reason_id'
+        return res
