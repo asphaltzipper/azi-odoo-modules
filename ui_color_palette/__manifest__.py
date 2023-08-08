bff5607d0275a769bdb64f99c80c5fec086dabc8
John Welch <jwelch@gmail.com>
2023-08-08 11:04:54 -0600

    Migrated ui_color_palette from v 12.0 to v 16.0

--- ui_color_palette/__manifest__.py
+++ ui_color_palette/__manifest__.py
@@ -3,22 +3,30 @@
 {
     'license': 'LGPL-3',
     "name": "Custom Color Palette",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Asphalt Zipper",
     "category": "Extra Tools",
     "summary": "Change Odoo color palette",
     'description': """
-    This module migrated from v 10.0 that is written by Bista Solutions to v 12.0 by Asphalt Zipper
-    For Odoo Enterprise Version 12.0, this module replaces the default Odoo purple color palette with a blue based 
+    This module migrated from v 10.0 that is written by Bista Solutions to v 16.0 by Asphalt Zipper
+    For Odoo Enterprise Version 16.0, this module replaces the default Odoo purple color palette with a blue based 
     color palette. By editing the CSS and SCSS files in this module, you can further personalize Odoo with 
     your own color choices. This is a very simple module. 
     It serves as an ideal example for those who are learning how to build modules with Odoo.""",
     "website": "http://www.github.com/asphaltzipper",
-    "depends": ["base"],
-    "demo_xml": [],
-    "data": [
-        'views/color_change.xml',
-    ],
+    "depends": ["web_enterprise"],
+    "assets": {
+        'web._assets_primary_variables': [
+            ('replace', 'web_enterprise/static/src/scss/primary_variables.scss', 'ui_color_palette/static/src/scss/variables.scss'),
+
+        ],
+        'web.assets_backend': [
+            'ui_color_palette/static/src/css/color_change.css',
+        ],
+        'web.assets_common': [
+            'ui_color_palette/static/src/scss/ui.scss'
+        ],
+    },
     "auto_install": False,
     "application": False,
     "installable": True,
