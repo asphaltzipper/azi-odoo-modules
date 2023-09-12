cc39177e40de0d71945989225e6cd3a24c3e5eab
John Welch <jwelch@gmail.com>
2023-09-12 10:13:24 -0600

    Fixed the issue with the domain in the sales region

--- sales_team_region/models/crm_team_region.py
+++ sales_team_region/models/crm_team_region.py
@@ -35,27 +35,9 @@ class CrmTeamRegion(models.Model):
         column1='region_id',
         column2='country_group_id')
 
-    # domain fields parameter appears to no longer be supported
-
-    # https://www.odoo.com/fr_FR/forum/aide-1/question/conditionally-apply-domain-on-a-field-25468
-    # https://www.odoo.com/fr_FR/groups/community-59/community-13935621?mode=thread&date_begin=&date_end=
-    # https://www.odoo.com/fr_FR/forum/aide-1/question/on-change-alternative-for-dynamic-domain-on-form-edit-15912
-    # https://www.odoo.com/fr_FR/forum/aide-1/question/filter-column-with-domain-and-funtion-72329
-    # http://stackoverflow.com/questions/30341476/is-there-any-way-to-call-a-function-when-a-record-is-requested-to-be-shown-in-a
-    # http://stackoverflow.com/questions/31825713/odoo-filter-many2one-field-on-load
-    # https://www.odoo.com/fr_FR/forum/aide-1/question/how-to-apply-domain-filter-on-many2many-field-28988
-    # workaround to filter sub-region lists on form view load as follows
-    # note: domain fields parameter must not be defined for this to work
-    def _compute_dom(self):
-        for record in self:
-            record.s_dom = self._region_domain(1)
-            record.c_dom = self._region_domain(2)
-            record.cg_dom = self._region_domain(3)
-
-    # https://gist.github.com/dreispt/a4c30fe671db2b2fce34
-    s_dom = fields.Many2many('res.country.state', compute=_compute_dom)
-    c_dom = fields.Many2many('res.country', compute=_compute_dom)
-    cg_dom = fields.Many2many('res.country.group', compute=_compute_dom)
+    s_dom = fields.Many2many('res.country.state', compute='_compute_dom')
+    c_dom = fields.Many2many('res.country', compute='_compute_dom')
+    cg_dom = fields.Many2many('res.country.group', compute='_compute_dom')
 
     # onchange concerns:
     # https://github.com/odoo/odoo/issues/11295#issuecomment-242644753
@@ -63,6 +45,48 @@ class CrmTeamRegion(models.Model):
     # other potential onchange concerns:
     # http://stackoverflow.com/questions/35840533/odoo-8-api-onchange-function-not-let-update-created-one2many-value/35841753#35841753
     # http://stackoverflow.com/questions/32326212/one2many-field-on-change-function-cant-change-its-own-value
+    def get_regions(self, state_ids, state_country_ids, country_ids, country_group_ids, states, countries, country_groups):
+        for state in states:
+            state_ids.add(state.id)
+            state_country_ids.add(state.country_id.id)
+        for country in countries:
+            country_ids.add(country.id)
+            for state in self.env['res.country.state'].search([('country_id', '=', country.id)]):
+                state_ids.add(state.id)  # exclude states of countries
+            for country_group in self.env['res.country'].search([('id', '=', country.id)]).country_group_ids:
+                country_group_ids.add(country_group.id)
+        for country_group in country_groups:
+            country_group_ids.add(country_group.id)
+            for country in country_group.country_ids:
+                country_ids.add(country.id)  # countries of country_groups
+                for state in self.env['res.country.state'].search([('country_id', '=', country.id)]):
+                    state_ids.add(state.id)
+        return state_ids, state_country_ids, country_ids, country_group_ids
+
+    def region_domain(self):
+        state_ids = set()
+        state_country_ids = set()
+        country_ids = set()
+        country_group_ids = set()
+        # add existing regions for domain exclusion
+        # look at optimizing this in the future
+        main_states, main_countries, main_groups = self.states._origin, self.countries._origin, self.country_groups._origin
+
+        for record in self.env['crm.team.region'].search([('region_types', 'in', self.region_types.ids), ('id', '!=', self._origin.id )]):
+            state_ids, state_country_ids, country_ids, country_group_ids = self.get_regions(state_ids,
+                                                                                            state_country_ids,
+                                                                                            country_ids,
+                                                                                            country_group_ids,
+                                                                                            record.states,
+                                                                                            record.countries,
+                                                                                            record.country_groups)
+        for country_id in state_country_ids:
+            country_ids.add(country_id)
+            for country_group in self.env['res.country'].search([('id', '=', country_id)]).country_group_ids:
+                country_group_ids.add(country_group.id)
+        state_ids, state_country_ids, country_ids, country_group_ids = self.get_regions(
+            state_ids, state_country_ids, country_ids, country_group_ids, main_states, main_countries, main_groups)
+        return set(state_ids), set(country_ids), set(country_group_ids)
 
     @api.constrains('states', 'countries', 'country_groups')
     def _require_region_definition(self):
@@ -74,78 +98,10 @@ class CrmTeamRegion(models.Model):
                                         " one state, country, or country"
                                         " group."))
 
-    def _region_domain(self, geoslice=False):
-        state_ids = set()
-        state_country_ids = set()
-        country_ids = set()
-        country_group_ids = set()
-        # add existing regions for domain exclusion
-        # look at optimizing this in the future
-        for record in self.env['crm.team.region'].search(
-                [('region_types', 'in', self.region_types.ids)]):
-            for state in record.states:
-                state_ids.add(state.id)
-                state_country_ids.add(state.country_id.id)
-            for country in record.countries:
-                country_ids.add(country.id)
-                for state in self.env['res.country.state'].search(
-                        [('country_id', '=', country.id)]):
-                    state_ids.add(state.id)  # exclude states of countries
-                for country_group in self.env['res.country'].search(
-                        [('id', '=', country.id)]).country_group_ids:
-                    country_group_ids.add(country_group.id)
-            for country_group in record.country_groups:
-                country_group_ids.add(country_group.id)
-                for country in country_group.country_ids:
-                    country_ids.add(country.id)  # countries of country_groups
-                    for state in self.env['res.country.state'].search(
-                            [('country_id', '=', country.id)]):
-                        state_ids.add(state.id)  # states of countries of etc.
-        for country_id in state_country_ids:
-            country_ids.add(country_id)
-            for country_group in self.env['res.country'].search(
-                    [('id', '=', country_id)]).country_group_ids:
-                country_group_ids.add(country_group.id)
-        region_domain = {1: list(state_ids), 2: list(country_ids),
-                         3: list(country_group_ids)}
-        if geoslice:
-            geoslice_value = region_domain.get(geoslice, [])
-            if geoslice == 1:
-                return self.env['res.country.state'].browse(geoslice_value)
-            elif geoslice == 2:
-                return self.env['res.country'].browse(geoslice_value)
-            return self.env['res.country.group'].browse(geoslice_value)
-        else:
-            return set(state_ids), set(country_ids), set(country_group_ids)
-
-    @api.onchange('country_groups', 'countries', 'states', 'region_types')
-    def onchange_region(self):
-        state_ids, country_ids, country_group_ids = self._region_domain()
-        state_country_ids = set()
-        # add currently selected regions for domain exclusion
-        for state in self.states:
-            for state_id in self.env['res.country.state'].browse(state.id):
-                state_country_ids.add(state_id.country_id.id)
-        for country_id in state_country_ids:
-            country_ids.add(country_id)
-            for country_group in self.env['res.country'].search(
-                    [('id', '=', country_id)]).country_group_ids:
-                country_group_ids.add(country_group.id)
-        for country in self.countries:
-            for state in self.env['res.country.state'].search(
-                    [('country_id', '=', country.id)]):
-                state_ids.add(state.id)
-            for country_group in self.env['res.country.group'].search(
-                    [('country_ids', 'in', country.id)]):
-                country_group_ids.add(country_group.id)
-        for country_group in self.country_groups:
-            for country in self.env['res.country.group'].browse(
-                    country_group.id)['country_ids']:
-                country_ids.add(country.id)
-                for state in self.env['res.country.state'].search(
-                        [('country_id', '=', country.id)]):
-                    state_ids.add(state.id)
-        return {'domain': {'states': [('id', 'not in', list(state_ids))],
-                           'countries': [('id', 'not in', list(country_ids))],
-                           'country_groups': [('id', 'not in',
-                                               list(country_group_ids))]}}
+    @api.depends('country_groups', 'countries', 'states', 'region_types')
+    def _compute_dom(self):
+        for record in self:
+            state_ids, country_ids, country_group_ids = record.region_domain()
+            record.s_dom = self.env['res.country.state'].search([('id', 'not in', list(state_ids))])
+            record.c_dom = self.env['res.country'].search([('id', 'not in', list(country_ids))])
+            record.cg_dom = self.env['res.country.group'].search([('id', 'not in', list(country_group_ids))])
