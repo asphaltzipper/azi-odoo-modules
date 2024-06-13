ed74b4cb06460ffc8b5721a409f1e942102c9028
John Welch <jwelch@gmail.com>
2024-06-13 07:07:18 -0600

    Modified sql statement to uninstall unnecessary modules

--- azi_migration/__init__.py
+++ azi_migration/__init__.py
@@ -3,5 +3,8 @@ def post_init_hook(cr, registry):
         UPDATE ir_module_module 
         SET state = 'to remove' 
         WHERE name in (
-            'product_quality', 'azi_web', 'account_tweaks', 'web_drag_drop_attach', 'mrp_production_merge', 'web_responsive') and state = 'installed'
+            'product_quality', 'azi_web', 'account_tweaks', 'web_drag_drop_attach', 
+            'mrp_production_merge', 'document_multi_upload', 'owncloud_odoo', 
+            'cloud_base', 'ywt_odoo_taxjar_connector', 'web_responsive
+        ) and state = 'installed'
     """)
