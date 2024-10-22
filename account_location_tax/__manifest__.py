8040ec9889d6e4e3c7feb611c9030df0fa100efd
Matt Taylor <matt454357@gmail.com>
2024-10-22 12:18:35 -0600

    fix install error on account_location_tax

--- account_location_tax/__manifest__.py
+++ account_location_tax/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # (c) 2018 John Welch
 
 {
     "name": "Account Location Tax",
-    "version": "1.0",
+    "version": "16.0.1.0.0",
     "summary": "Display sales revenue by zip code",
     "category": "Accounting",
     "author": "John Welch",
