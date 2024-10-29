24e0e29dcae2c4534c12d608be8910fbe0e9c213
John Welch <jwelch@gmail.com>
2024-10-29 10:08:26 -0600

    Fixed the unit cost in the valuation and displayed inventory_value as null in the view

--- azi_stock/models/product.py
+++ azi_stock/models/product.py
@@ -31,7 +31,7 @@ class ProductProduct(models.Model):
     def _prepare_in_svl_vals(self, quantity, unit_cost):
         res = super(ProductProduct, self)._prepare_in_svl_vals(quantity, unit_cost)
         inventory_value = self.env.context.get('inventory_value', False)
-        if self.env.context.get('is_quant', False) and inventory_value:
+        if self.env.context.get('is_quant', False):
             company_id = self.env.context.get('force_company', self.env.company.id)
             company = self.env['res.company'].browse(company_id)
             value = company.currency_id.round(inventory_value * quantity)
