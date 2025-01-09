95b21db68552d4889878d0e2ac8e91480356ecfd
Matt Taylor <matt454357@gmail.com>
2025-01-08 19:17:23 -0700

    fix field name conflict
    We renamed scheduled_date to sr_date.
    
    The field, sale.order.line.scheduled_date, is created by the sale_stock module
    in 16.0.  This conflicts with our stock_request_schedule module.

--- stock_request_schedule/models/sale_order.py
+++ stock_request_schedule/models/sale_order.py
@@ -11,15 +11,15 @@ class SaleOrderLine(models.Model):
         readonly=True,
     )
 
-    is_scheduled = fields.Boolean(
-        string='Scheduled',
+    is_on_sr = fields.Boolean(
+        string='On Stock Request',
         compute='_compute_scheduled',
         store=True,
         compute_sudo=True
     )
 
-    scheduled_date = fields.Datetime(
-        string='Scheduled Date',
+    sr_date = fields.Datetime(
+        string='Stock Request Date',
         compute='_compute_scheduled',
         compute_sudo=True
     )
@@ -28,5 +28,5 @@ class SaleOrderLine(models.Model):
     def _compute_scheduled(self):
         for line in self:
             stock_request = line.stock_request_ids.filtered(lambda x: x.state in ['draft', 'submitted', 'open'])
-            line.is_scheduled = len(stock_request) and True or False
-            line.scheduled_date = stock_request and stock_request[0].expected_date
+            line.is_on_sr = len(stock_request) and True or False
+            line.sr_date = stock_request and stock_request[0].expected_date or False
