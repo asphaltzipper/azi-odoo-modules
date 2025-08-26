8cbe639c7eb7c773f7613da558797fc001d543e5
John Welch <jwelch@gmail.com>
2025-08-26 10:42:22 -0600

    Modified the next serial button in the MO

--- azi_mrp/models/mrp_production.py
+++ azi_mrp/models/mrp_production.py
@@ -303,3 +303,26 @@ class MrpProduction(models.Model):
         """, (lang, quantity, bom_id, lang)
         )
         return self._cr.fetchall()
+
+    def action_generate_serial(self):
+        self.ensure_one()
+        self.lot_producing_id = self.env['stock.lot'].create(self._prepare_stock_lot_values_based_on_sequence())
+        if self.product_id.tracking == 'serial':
+            self._set_qty_producing()
+
+    def _prepare_stock_lot_values_based_on_sequence(self):
+        self.ensure_one()
+        name = self.env['ir.sequence'].next_by_code('stock.lot.serial')
+        exist_lot = self.env['stock.lot'].search([
+            ('product_id', '=', self.product_id.id),
+            ('company_id', '=', self.company_id.id),
+            ('name', '=', name),
+        ], limit=1)
+        if exist_lot:
+            name = self.env['stock.lot']._get_next_serial(self.company_id, self.product_id)
+
+        return {
+            'product_id': self.product_id.id,
+            'company_id': self.company_id.id,
+            'name': name,
+        }
