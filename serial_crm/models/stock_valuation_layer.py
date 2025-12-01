00896231b032e557ec636b157d598de832f526e7
Matt Taylor <matt454357@gmail.com>
2025-12-01 10:43:47 -0700

    fix duplicate method def

--- serial_crm/models/stock_valuation_layer.py
+++ serial_crm/models/stock_valuation_layer.py
@@ -1,6 +1,3 @@
-# Copyright 2023 Ecosoft Co., Ltd (https://ecosoft.co.th)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-
 from odoo import fields, models
 
 
@@ -12,9 +9,3 @@ class StockValuationLayer(models.Model):
         string="Post Date",
         store=True,
     )
-
-    def _get_unconsumed_in_move_line(self, lot):
-        self.ensure_one()
-        return self.stock_move_id.move_line_ids.filtered(
-            lambda x: x.lot_id == lot and x.qty_remaining
-        )
