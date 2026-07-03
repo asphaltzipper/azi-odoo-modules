65919772c1ff163923fd0a5ffd97ca5c3b903208
Matt Taylor <matt454357@gmail.com>
2026-07-03 10:38:14 -0600

    fix minor log error when using ext_produce()

--- mrp_wo_produce/models/mrp_production.py
+++ mrp_wo_produce/models/mrp_production.py
@@ -109,15 +109,16 @@ class MrpProduction(models.Model):
 
     def ext_produce(self):
         """
-            Complete the MO
-            Callable from OdooRPC
-            The calling app must handle labor and serial numbers
+        Complete the MO
+        Callable from OdooRPC
+        The calling app must handle labor and serial numbers
         """
         self.ensure_one()
         self.qty_producing = self.product_qty
         self._set_qty_producing()
         self.create_workorder_labor()
         self.button_mark_done()
+        return True
 
 
 class MrpProductionLabor(models.Model):
